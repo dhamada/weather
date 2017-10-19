86f7eee25a20ba5344aa7d1bb79f22487ad41a03
hamada taigen <t.hamada@nt21.jp>
2017-10-19 19:05:09 +0900

    update

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const getWeatherReport = () => {
-    return axios.get('http://weathernews.jp/onebox/35.689876/139.691670/temp=c&q=%E6%9D%B1%E4%BA%AC%E9%83%BD%E5%BA%81')
+    return axios.get('https://weathernews.jp/onebox/35.689876/139.691670/temp=c&q=%E6%9D%B1%E4%BA%AC%E' +
+            '9%83%BD%E5%BA%81')
 }
